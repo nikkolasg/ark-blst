abe64b5856cb592553553abd25efe404551323ee
nikkolasg <nikkolasg@gmail.com>
2023-01-15 01:27:30 +0100

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use core::{
     fmt,
     ops::{Add, AddAssign, Mul, MulAssign, Neg, Sub, SubAssign},
 };
+use std::io::Empty;
 
 use ark_serialize::{
     buffer_byte_size, CanonicalDeserialize, CanonicalDeserializeWithFlags, CanonicalSerialize,
@@ -136,15 +137,6 @@ impl Mul<Fp> for Fp {
     }
 }
 
-impl Mul<&Fp> for Fp {
-    type Output = Fp;
-
-    #[inline]
-    fn mul(mut self, rhs: &Fp) -> Fp {
-        self.0.mul_assign(rhs.0);
-        self
-    }
-}
 impl Mul<&Fp> for &Fp {
     type Output = Fp;
 
@@ -184,138 +176,196 @@ impl Hash for Fp {
     }
 }
 
-impl ark_ff::Field for Fp {
-    type BasePrimeField = Fp;
-
-    type BasePrimeFieldIter = iter::Once<Self::BasePrimeField>;
-
-    const SQRT_PRECOMP: Option<ark_ff::SqrtPrecomputation<Self>> = None;
-
-    const ZERO: Self = Fp::zero();
-
-    const ONE: Self = Fp::one();
-
-    fn extension_degree() -> u64 {
-        1
+impl ark_serialize::CanonicalSerialize for Fp {
+    fn serialize_with_mode<W: ark_serialize::Write>(
+        &self,
+        writer: W,
+        compress: Compress,
+    ) -> Result<(), SerializationError> {
+        let buff = self.0.to_bytes_le();
+        writer.write(&buff[..])?;
+        Ok(())
     }
 
-    fn to_base_prime_field_elements(&self) -> Self::BasePrimeFieldIter {
-        iter::once(*self)
-    }
-
-    fn from_base_prime_field_elems(elems: &[Self::BasePrimeField]) -> Option<Self> {
-        if elems.len() != (Self::extension_degree() as usize) {
-            return None;
-        }
-        Some(elems[0])
-    }
-
-    fn from_base_prime_field(elem: Self::BasePrimeField) -> Self {
-        elem
-    }
-
-    #[inline]
-    fn double(&self) -> Self {
-        Fp(self.0.double())
-    }
-
-    fn double_in_place(&mut self) -> &mut Self {
-        self.0 = self.0.double()
-    }
-
-    fn neg_in_place(&mut self) -> &mut Self {
-        self.0 = self.0.neg();
-    }
-
-    fn from_random_bytes_with_flags<F: Flags>(bytes: &[u8]) -> Option<(Self, F)> {
-        let blst_buffer = [0u8; 48];
-        blst_buffer[..].copy_from_slice(bytes);
-        blstrs::Fp::from_bytes_le(&blst_buffer);
-    }
-
-    fn legendre(&self) -> ark_ff::LegendreSymbol {
-        todo!()
-    }
-
-    fn square(&self) -> Self {
-        Fp(self.0.square())
-    }
-
-    fn square_in_place(&mut self) -> &mut Self {
-        self.0 = self.0.square();
-    }
-
-    fn inverse(&self) -> Option<Self> {
-        self.0.invert()
-    }
-
-    fn inverse_in_place(&mut self) -> Option<&mut Self> {
-        self.0 = self.0.invert();
-    }
-
-    fn frobenius_map_in_place(&mut self, power: usize) {
-        todo!()
-    }
-
-    fn characteristic() -> &'static [u64] {
-        Self::BasePrimeField::characteristic()
-    }
-
-    fn from_random_bytes(bytes: &[u8]) -> Option<Self> {
-        Self::from_random_bytes_with_flags::<EmptyFlags>(bytes).map(|f| f.0)
-    }
-
-    fn sqrt(&self) -> Option<Self> {
-        match Self::SQRT_PRECOMP {
-            Some(tv) => tv.sqrt(self),
-            None => self.0.sqrt(),
-        }
-    }
-
-    fn sqrt_in_place(&mut self) -> Option<&mut Self> {
-        (*self).sqrt().map(|sqrt| {
-            *self = sqrt;
-            self
-        })
-    }
-
-    fn sum_of_products<const T: usize>(a: &[Self; T], b: &[Self; T]) -> Self {
-        let mut sum = Self::zero();
-        for i in 0..a.len() {
-            sum += a[i] * b[i];
-        }
-        sum
-    }
-
-    fn frobenius_map(&self, power: usize) -> Self {
-        let mut this = *self;
-        this.frobenius_map_in_place(power);
-        this
-    }
-
-    fn pow<S: AsRef<[u64]>>(&self, exp: S) -> Self {
-        let mut res = Self::one();
-
-        for i in ark_ff::BitIteratorBE::without_leading_zeros(exp) {
-            res.square_in_place();
-
-            if i {
-                res *= self;
-            }
-        }
-        res
-    }
-
-    fn pow_with_table<S: AsRef<[u64]>>(powers_of_2: &[Self], exp: S) -> Option<Self> {
-        let mut res = Self::one();
-        for (pow, bit) in ark_ff::BitIteratorLE::without_trailing_zeros(exp).enumerate() {
-            if bit {
-                res *= powers_of_2.get(pow)?;
-            }
-        }
-        Some(res)
+    fn serialized_size(&self, compress: Compress) -> usize {
+        48
     }
 }
+
+impl ark_serialize::CanonicalSerializeWithFlags for Fp {
+    fn serialize_with_flags<W: ark_serialize::Write, F: Flags>(
+        &self,
+        writer: W,
+        flags: F,
+    ) -> Result<(), SerializationError> {
+        self.serialize_with_mode(writer, Compress::No)
+    }
+
+    fn serialized_size_with_flags<F: Flags>(&self) -> usize {
+        self.serialized_size(Compress::No)
+    }
+}
+
+impl ark_serialize::Valid for Fp {
+    fn check(&self) -> Result<(), SerializationError> {
+        Ok(())
+    }
+}
+
+impl ark_serialize::CanonicalDeserialize for Fp {
+    fn deserialize_with_mode<R: ark_serialize::Read>(
+        reader: R,
+        compress: Compress,
+        validate: Validate,
+    ) -> Result<Self, SerializationError> {
+        let mut buff = [0u8; 48];
+        reader.read(&mut buff[..])?;
+        Option::from(blstrs::Fp::from_bytes_le(&buff))
+            .map(|k| Fp(k))
+            .ok_or(SerializationError::InvalidData)
+    }
+}
+
+impl ark_serialize::CanonicalDeserializeWithFlags for Fp {
+    fn deserialize_with_flags<R: ark_serialize::Read, F: Flags>(
+        reader: R,
+    ) -> Result<(Self, F), SerializationError> {
+        Fr::from_u8(0).ok_or(SerializationError::InvalidData).map(|flag| Fp::deserialize_with_mode(reader, Compress::No, Validate::No).map(|elem| (elem,flag)))
+    }
+}
+
+//impl ark_ff::Field for Fp {
+//    type BasePrimeField = Fp;
+//
+//    type BasePrimeFieldIter = iter::Once<Self::BasePrimeField>;
+//
+//    const SQRT_PRECOMP: Option<ark_ff::SqrtPrecomputation<Self>> = None;
+//
+//    const ZERO: Self = Fp::zero();
+//
+//    const ONE: Self = Fp::one();
+//
+//    fn extension_degree() -> u64 {
+//        1
+//    }
+//
+//    fn to_base_prime_field_elements(&self) -> Self::BasePrimeFieldIter {
+//        iter::once(*self)
+//    }
+//
+//    fn from_base_prime_field_elems(elems: &[Self::BasePrimeField]) -> Option<Self> {
+//        if elems.len() != (Self::extension_degree() as usize) {
+//            return None;
+//        }
+//        Some(elems[0])
+//    }
+//
+//    fn from_base_prime_field(elem: Self::BasePrimeField) -> Self {
+//        elem
+//    }
+//
+//    #[inline]
+//    fn double(&self) -> Self {
+//        Fp(self.0.double())
+//    }
+//
+//    fn double_in_place(&mut self) -> &mut Self {
+//        self.0 = self.0.double()
+//    }
+//
+//    fn neg_in_place(&mut self) -> &mut Self {
+//        self.0 = self.0.neg();
+//    }
+//
+//    fn from_random_bytes_with_flags<F: Flags>(bytes: &[u8]) -> Option<(Self, F)> {
+//        let blst_buffer = [0u8; 48];
+//        blst_buffer[..].copy_from_slice(bytes);
+//        blstrs::Fp::from_bytes_le(&blst_buffer);
+//    }
+//
+//    fn legendre(&self) -> ark_ff::LegendreSymbol {
+//        todo!()
+//    }
+//
+//    fn square(&self) -> Self {
+//        Fp(self.0.square())
+//    }
+//
+//    fn square_in_place(&mut self) -> &mut Self {
+//        self.0 = self.0.square();
+//    }
+//
+//    fn inverse(&self) -> Option<Self> {
+//        self.0.invert()
+//    }
+//
+//    fn inverse_in_place(&mut self) -> Option<&mut Self> {
+//        self.0 = self.0.invert();
+//    }
+//
+//    fn frobenius_map_in_place(&mut self, power: usize) {
+//        todo!()
+//    }
+//
+//    fn characteristic() -> &'static [u64] {
+//        Self::BasePrimeField::characteristic()
+//    }
+//
+//    fn from_random_bytes(bytes: &[u8]) -> Option<Self> {
+//        Self::from_random_bytes_with_flags::<EmptyFlags>(bytes).map(|f| f.0)
+//    }
+//
+//    fn sqrt(&self) -> Option<Self> {
+//        match Self::SQRT_PRECOMP {
+//            Some(tv) => tv.sqrt(self),
+//            None => self.0.sqrt(),
+//        }
+//    }
+//
+//    fn sqrt_in_place(&mut self) -> Option<&mut Self> {
+//        (*self).sqrt().map(|sqrt| {
+//            *self = sqrt;
+//            self
+//        })
+//    }
+//
+//    fn sum_of_products<const T: usize>(a: &[Self; T], b: &[Self; T]) -> Self {
+//        let mut sum = Self::zero();
+//        for i in 0..a.len() {
+//            sum += a[i] * b[i];
+//        }
+//        sum
+//    }
+//
+//    fn frobenius_map(&self, power: usize) -> Self {
+//        let mut this = *self;
+//        this.frobenius_map_in_place(power);
+//        this
+//    }
+//
+//    fn pow<S: AsRef<[u64]>>(&self, exp: S) -> Self {
+//        let mut res = Self::one();
+//
+//        for i in ark_ff::BitIteratorBE::without_leading_zeros(exp) {
+//            res.square_in_place();
+//
+//            if i {
+//                res *= self;
+//            }
+//        }
+//        res
+//    }
+//
+//    fn pow_with_table<S: AsRef<[u64]>>(powers_of_2: &[Self], exp: S) -> Option<Self> {
+//        let mut res = Self::one();
+//        for (pow, bit) in ark_ff::BitIteratorLE::without_trailing_zeros(exp).enumerate() {
+//            if bit {
+//                res *= powers_of_2.get(pow)?;
+//            }
+//        }
+//        Some(res)
+//    }
+//}
 
 #[cfg(test)]
 mod tests {
