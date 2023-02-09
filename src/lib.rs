b7915f8597a279cdd2d1932c3a1ae41bde022df1
nikkolasg <nikkolasg@gmail.com>
2023-02-09 13:26:53 +0100

    crate public facing

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,11 @@ pub mod fp;
 pub(crate) mod fp12;
 pub(crate) mod fp2;
 pub(crate) mod fp6;
-pub mod g1;
-pub mod g2;
+pub(crate) mod g1;
+pub(crate) mod g2;
 pub(crate) mod memory;
-pub mod pairing;
-pub mod scalar;
+pub(crate) mod pairing;
+pub(crate) mod scalar;
 
 pub use fp12::Fp12 as Gt;
 pub use g1::{G1Affine, G1Projective};
